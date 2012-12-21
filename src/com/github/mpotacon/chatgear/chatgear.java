c155db387e33278565283e1aea7163c319c5866e
mpotacon <mpotacon@aol.com>
2012-12-20 23:48:28 -0500

    Checks for Permissions

--- src/com/github/mpotacon/chatgear/chatgear.java
+++ src/com/github/mpotacon/chatgear/chatgear.java
@@ -1,30 +1,28 @@
 package com.github.mpotacon.chatgear;
 
-//import org.bukkit.entity.Player;
-//import org.bukkit.event.EventHandler;
-//import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
-
-//import ru.tehkode.permissions.PermissionUser;
-//import ru.tehkode.permissions.bukkit.PermissionsEx;
 
 public final class chatgear extends JavaPlugin {
 	public static chatgear plugin;
 	public final chatListener cl = new chatListener();
+	public final chatPEx cPEx = new chatPEx();
+	PluginManager pm = getServer().getPluginManager();
 	
 	@Override
 	public void onEnable(){
 		getCommand("tell").setExecutor(new tellCommand());
 		getCommand("ping").setExecutor(new pingCommand());
 		getCommand("mute").setExecutor(new muteCommand());
-		PluginManager pm = getServer().getPluginManager();
-		pm.registerEvents(this.cl, this);
-	}
+		if(pm.isPluginEnabled("PermissionsEx")){
+			pm.registerEvents(this.cPEx, this);
+		}else{
+			pm.registerEvents(this.cl, this);
+		}		
+	}	
 	
 	@Override
 	public void onDisable(){
+		
 	}
-	
-
 }
