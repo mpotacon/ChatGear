b0ee4a3021d9226c16bbc2fd7aa50d2d39a3187c
Master Programmer Otacon <mpotacon>
2012-12-24 01:21:23 -0500

    More PEx config

--- src/com/github/mpotacon/chatgear/chatPEx.java
+++ src/com/github/mpotacon/chatgear/chatPEx.java
@@ -2,6 +2,7 @@ package com.github.mpotacon.chatgear;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -18,10 +19,18 @@ public class chatPEx implements Listener {
 	
 	public final static HashMap<String, ArrayList<Block>> mutedPlayers = new HashMap<String, ArrayList<Block>>();
 	public static chatgear plugin;
-
+	private int i;
+	private String[] group;
+	private String[] groupcolor;
+	private String[] groupchatcolor;
+	
 	@EventHandler
 	public void onPlayerChatEvent(AsyncPlayerChatEvent event){
 		Player player = event.getPlayer();
+		PermissionUser user = PermissionsEx.getUser(player);
+		@SuppressWarnings("unused")
+		boolean isInGroup = user.inGroup(group[i]);
+		
 		if(mutedPlayers.containsKey(player.getName())){
 			try{
 			event.setCancelled(true);
@@ -30,93 +39,36 @@ public class chatPEx implements Listener {
 				event.setMessage("");
 			}
 		}
-		String OP = ChatColor.translateAlternateColorCodes('&', "&" + Bukkit.getPluginManager().getPlugin("chatgear").getConfig().getString("OP.color") + player.getDisplayName());
-	 	String OPcolor = ChatColor.translateAlternateColorCodes('&', "&" + Bukkit.getPluginManager().getPlugin("chatgear").getConfig().getString("OP.chatcolor") + event.getMessage());
-	 	String nonOP = ChatColor.translateAlternateColorCodes('&', "&" + Bukkit.getPluginManager().getPlugin("chatgear").getConfig().getString("player.color") + player.getDisplayName());
-	 	String nonOPcolor = ChatColor.translateAlternateColorCodes('&', "&" + Bukkit.getPluginManager().getPlugin("chatgear").getConfig().getString("player.chatcolor") + event.getMessage());
 	 	String symbol =  ChatColor.translateAlternateColorCodes('&', Bukkit.getPluginManager().getPlugin("chatgear").getConfig().getString("break"));
-				
-		PermissionUser user = PermissionsEx.getUser(player);
-	 	String prefix = ChatColor.translateAlternateColorCodes('&', user.getOwnPrefix());
+	 	String PEx = Bukkit.getPluginManager().getPlugin("chatgear").getConfig().getString("PExColor");
+			
+	 	String name = player.getDisplayName();
+		String prefix = ChatColor.translateAlternateColorCodes('&', user.getOwnPrefix());
 	 	String suffix = ChatColor.translateAlternateColorCodes('&', user.getOwnSuffix());
-	 	String admin =  Bukkit.getPluginManager().getPlugin("chatgear").getConfig().getString("AdminGroup");
-	 	String mod = Bukkit.getPluginManager().getPlugin("chatgear").getConfig().getString("ModGroup");
-	 	if(user.inGroup(admin)){
-	 		if(prefix != null & suffix != null){
-	 			if (OP == "PEx"){
-	 				event.setFormat(player.getDisplayName() + symbol + " "+ OPcolor + ChatColor.translateAlternateColorCodes('&', event.getMessage()));
-	 			}else{
-	 				event.setFormat(OP + symbol + " " + OPcolor + ChatColor.translateAlternateColorCodes('&', event.getMessage()));
-	 			}
-	 		}else if(prefix != null){
-	 			if (OP == "PEx"){	
-	 				event.setFormat(player.getDisplayName() + suffix + " " + symbol + " " + OPcolor + ChatColor.translateAlternateColorCodes('&', event.getMessage()));
-	 			}else{
-	 				event.setFormat(OP + suffix + " " + symbol + " " + OPcolor + ChatColor.translateAlternateColorCodes('&', event.getMessage()));
-	 			}
-	 		}else if(suffix != null){
-	 			if (OP == "PEx"){
-	 				event.setFormat(prefix + player.getDisplayName() + symbol + " "+ OPcolor + ChatColor.translateAlternateColorCodes('&', event.getMessage()));
-	 			}else{
-	 				event.setFormat(prefix + OP + symbol + " " + OPcolor + ChatColor.translateAlternateColorCodes('&', event.getMessage()));
-	 			}
+
+	 	for(int i = 0; i < i; i++){
+	 		group[i] = 	Bukkit.getPluginManager().getPlugin("chatgear").getConfig().getString("PEx.Group" + i);
+	 		groupcolor[i] = ChatColor.translateAlternateColorCodes('&', "&" + Bukkit.getPluginManager().getPlugin("chatgear").getConfig().getString("PEx.Group" + i + ".color"));
+	 		groupchatcolor[i] = ChatColor.translateAlternateColorCodes('&', "&" + Bukkit.getPluginManager().getPlugin("chatgear").getConfig().getString("PEx.Group" + i + ".chatcolor"));
+	 	}
+	 	if(prefix != null ){
+	 		if (PEx == "Yes" || PEx == "YES"){
+	 			event.setFormat(name + suffix + " " + ChatColor.translateAlternateColorCodes('&', event.getMessage()));
 	 		}else{
-	 			if (OP == "PEx"){
-	 				event.setFormat(prefix + player.getDisplayName() + suffix + symbol + " "+ OPcolor + ChatColor.translateAlternateColorCodes('&', event.getMessage()));
-	 			}else{
-	 				event.setFormat(prefix + OP + suffix + symbol + " " + OPcolor + ChatColor.translateAlternateColorCodes('&', event.getMessage()));
-	 			}
+	 			event.setFormat(groupcolor[i] + name + symbol + " " + groupchatcolor[i] + ChatColor.translateAlternateColorCodes('&', event.getMessage()));
 	 		}
-	 	}else if(user.inGroup(mod)){
-	 		if(prefix != null & suffix != null){
-	 			if (OP == "PEx"){
-	 				event.setFormat(player.getDisplayName() + symbol + " "+ OPcolor + ChatColor.translateAlternateColorCodes('&', event.getMessage()));
-	 			}else{
-	 				event.setFormat(OP + symbol + " " + OPcolor + ChatColor.translateAlternateColorCodes('&', event.getMessage()));
-	 			}
-	 		}else if(prefix != null){
-	 			if (OP == "PEx"){	
-	 				event.setFormat(player.getDisplayName() + suffix + " " + symbol + " " + OPcolor + ChatColor.translateAlternateColorCodes('&', event.getMessage()));
-	 			}else{
-	 				event.setFormat(OP + suffix + " " + symbol + " " + OPcolor + ChatColor.translateAlternateColorCodes('&', event.getMessage()));
-	 			}
-	 		}else if(suffix != null){
-	 			if (OP == "PEx"){
-	 				event.setFormat(prefix + player.getDisplayName() + symbol + " "+ OPcolor + ChatColor.translateAlternateColorCodes('&', event.getMessage()));
-	 			}else{
-	 				event.setFormat(prefix + OP + symbol + " " + OPcolor + ChatColor.translateAlternateColorCodes('&', event.getMessage()));
-	 			}
-	 		}else{
-	 			if (OP == "PEx"){
-	 				event.setFormat(prefix + player.getDisplayName() + suffix + symbol + " "+ OPcolor + ChatColor.translateAlternateColorCodes('&', event.getMessage()));
-	 			}else{
-	 				event.setFormat(prefix + OP + suffix + symbol + " " + OPcolor + ChatColor.translateAlternateColorCodes('&', event.getMessage()));
-	 			}
-	 		}
-	 	}else if(prefix != null & suffix != null){
-	 		if (nonOP == "PEx"){
- 				event.setFormat(player.getDisplayName() + symbol + " "+ nonOPcolor + ChatColor.translateAlternateColorCodes('&', event.getMessage()));
- 			}else{
- 				event.setFormat(nonOP + symbol + " " + nonOPcolor + ChatColor.translateAlternateColorCodes('&', event.getMessage()));
- 			}
-	 	}else if(prefix != null){
-	 		if (nonOP == "PEx"){	
- 				event.setFormat(player.getDisplayName() + suffix + " " + symbol + " " + nonOPcolor + ChatColor.translateAlternateColorCodes('&', event.getMessage()));
- 			}else{
- 				event.setFormat(nonOP + suffix + " " + symbol + " " + nonOPcolor + ChatColor.translateAlternateColorCodes('&', event.getMessage()));
- 			}
 	 	}else if(suffix != null){
-	 		if (nonOP == "PEx"){
- 				event.setFormat(prefix + player.getDisplayName() + symbol + " "+ nonOPcolor + ChatColor.translateAlternateColorCodes('&', event.getMessage()));
- 			}else{
- 				event.setFormat(prefix + nonOP + symbol + " " + nonOPcolor + ChatColor.translateAlternateColorCodes('&', event.getMessage()));
- 			}
+	 		if (PEx == "Yes" || PEx == "YES"){
+	 			Logger.getLogger("Minecraft").info("[chatgear] config.yml Error: PExColor= Yes but PermissionsEx has no suffix");
+	 		}else{
+	 			event.setFormat(prefix + groupcolor[i] + name + symbol + " " + groupchatcolor[i] + ChatColor.translateAlternateColorCodes('&', event.getMessage()));
+	 		}
 	 	}else{
-	 		if (nonOP == "PEx"){
- 				event.setFormat(prefix + player.getDisplayName() + suffix + symbol + " "+ nonOPcolor + ChatColor.translateAlternateColorCodes('&', event.getMessage()));
- 			}else{
- 				event.setFormat(prefix + nonOP + suffix + symbol + " " + nonOPcolor + ChatColor.translateAlternateColorCodes('&', event.getMessage()));
- 			}
+	 		if (PEx == "Yes" || PEx == "YES"){
+	 			event.setFormat(prefix + name + suffix + symbol + " " + ChatColor.translateAlternateColorCodes('&', event.getMessage()));
+	 		}else{
+	 			event.setFormat(prefix + groupcolor[i] + name + suffix + symbol + " " + groupchatcolor[i] + ChatColor.translateAlternateColorCodes('&', event.getMessage()));
+	 		}
 	 	}
 	 }
 }
